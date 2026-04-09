94458318c0ac518070a33238ace5c6c4134f1c1b
Claudio DeSouza <cdesouza@brave.com>
2026-04-09 13:58:02 +0100

    Revert "[cr148][REVERT-ME] Brave pinsets broken"
    This reverts commit c363e4ef84ee8e37f713b4e9b1f4720270f3e2cc.

--- chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
+++ chromium_src/net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -5,11 +5,11 @@
 
 #include <string_view>
 
-// #define ParseJSON ParseJSON_ChromiumImpl
-// #define ParseCertificatesFile ParseCertificatesFile_ChromiumImpl
+#define ParseJSON ParseJSON_ChromiumImpl
+#define ParseCertificatesFile ParseCertificatesFile_ChromiumImpl
 #include <net/tools/transport_security_state_generator/input_file_parsers.cc>
-// #undef ParseCertificatesFile
-// #undef ParseJSON
+#undef ParseCertificatesFile
+#undef ParseJSON
 
 namespace {
 // NOTE: Do not add any host which has TLS terminated by Cloudflare.
@@ -546,9 +546,9 @@ constexpr std::string_view kBraveHstsJson = R"brave_hsts_json({
 
 namespace net::transport_security_state {
 
-bool ParseCertificatesFileToFix(std::string_view certs_input,
-                                Pinsets* pinsets,
-                                base::Time* timestamp) {
+bool ParseCertificatesFile(std::string_view certs_input,
+                           Pinsets* pinsets,
+                           base::Time* timestamp) {
   constexpr std::string_view brave_certs = R"brave_certs(
 # Last updated: Thu Apr 09 13:19:05 2026
 PinsListTimestamp
@@ -1261,25 +1261,24 @@ csOPNIXeJm1YsnJyxUGvKRWjOn+vC9k1SXilnnwhcIs9Pp9e5bckYAnB79VJXN/L
 # =====END BRAVE ROOTS ASC=====
 )brave_certs";
 
-  return ParseCertificatesFile(brave_certs, pinsets, timestamp);
+  return ParseCertificatesFile_ChromiumImpl(brave_certs, pinsets, timestamp);
 }
 
-bool ParseJSONToFix(std::string_view hsts_json,
-                    std::string_view pins_json,
-                    TransportSecurityStateEntries* entries,
-                    PinEntries* pin_entries,
-                    Pinsets* pinsets) {
+bool ParseJSON(std::string_view hsts_json,
+               std::string_view pins_json,
+               TransportSecurityStateEntries* entries,
+               Pinsets* pinsets) {
   Pinsets chromium_pinsets;
   TransportSecurityStateEntries chromium_entries;
-  if (!ParseJSON(hsts_json, pins_json, &chromium_entries, pin_entries,
-                 &chromium_pinsets)) {
+  if (!ParseJSON_ChromiumImpl(hsts_json, pins_json, &chromium_entries,
+                              &chromium_pinsets)) {
     return false;
   }
 
   for (auto& entry : chromium_entries) {
     // Google has asked us not to include the pins that ship with Chrome,
     // but we do want the preloaded HSTS entries.
-    // entry->pinset = "";
+    entry->pinset = "";
     if (!entry->force_https) {
       continue;
     }
@@ -1287,8 +1286,8 @@ bool ParseJSONToFix(std::string_view hsts_json,
     entries->push_back(std::move(entry));
   }
 
-  return ParseJSON(kBraveHstsJson, kBravePinsJson, entries, pin_entries,
-                   pinsets);
+  return ParseJSON_ChromiumImpl(kBraveHstsJson, kBravePinsJson, entries,
+                                pinsets);
 }
 
 }  // namespace net::transport_security_state
